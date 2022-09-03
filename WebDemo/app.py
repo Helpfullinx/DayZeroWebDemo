5fd3a1e9896e80c23933d7766867e717006808f6
Zach van Welzen <zachvdub2002@gmail.com>
2022-09-02 20:38:30 -0500

    Merged all files and Implemented login screen

--- WebDemo/app.py
+++ WebDemo/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template()
+    return render_template('DayZeroWebsite (2).html')
 
 
 if __name__ == '__main__':
